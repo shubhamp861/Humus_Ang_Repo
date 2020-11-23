870ef541552fee432cd8544d97436ea4643c518d
shubhamp861 <sp7075892@gmail.com>
2020-11-23 05:07:24 -0800

    Fixing month,day,weeks issue.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'humus';
+  today = new Date();
+  monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  month = '';
+  week = '';
+  date = '';
+  constructor() {
+    this.date = formatDate(this.today, 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+0530').slice(0, 2);
+    this.month = this.monthNames[this.today.getMonth()].slice(0, 3);
+    this.week = this.weeks[this.today.getDay()];
+
+  }
 }
